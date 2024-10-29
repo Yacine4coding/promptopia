3d3db446b9addce02fff4fca24ba8ce7e546f893
Yacine4coding <bensidahmedyacine.off@gmail.com>
2024-10-29 19:58:49 +0100

    add experimental to next config

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ const nextConfig = {
         hostname: 'lh3.googleusercontent.com',
       },
     ],
+  },  
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
   },
   webpack(config) {
     config.experiments = {
